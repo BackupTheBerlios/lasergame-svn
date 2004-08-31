ad6041abcc2125eab9a65aa021416e7819a77482
zwin <zwin@72742d07-e5df-0310-90a3-f1be50577a5f>
2004-08-31 17:26:50 +0000

    Adding util/math.h (compatibility layer).

---
+++ trunk/util/math.h
@@ -0,0 +1,29 @@
+#ifndef UTIL_MATH_H_INCLUDED
+#define UTIL_MATH_H_INCLUDED 1
+
+#include <float.h>
+#include <math.h>
+
+#ifdef _WIN32
+	
+	inline double copysign (double x, double y) { return _copysign(x,y); }
+	inline double chgsign (double x) { return _chgsign(x); }
+	inline double scalb(double x, long y){ return _scalb(x,y); }
+//	inline double logb(double x){ return _logb(x); }
+	inline double nextafter(double x, double y){ return _nextafter(x, y); }
+	inline int    finite(double x){ return _finite(x); }
+	inline int    isnan(double x){ return _isnan(x); }
+	inline int    fpclass(double x){ return _fpclass(x); }
+
+#	ifdef _MSC_VER
+#		define M_SQRT2	1.41421356237309504880	/* sqrt(2) */
+#		define M_PI 3.1415926535897932384626433832795
+#	endif
+
+#elif defined linux
+
+#endif
+
+#endif //MY_MATH_H_INCLUDED
+
+
