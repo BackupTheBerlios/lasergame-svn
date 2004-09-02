6151fdb859d170fb884f06b3caac615a3abee7e2
zwin <zwin@72742d07-e5df-0310-90a3-f1be50577a5f>
2004-09-02 11:23:49 +0000

    Importing time stuff.

---
+++ trunk/number/time-test.cpp
@@ -0,0 +1,16 @@
+/** @file
+*                  Tests for Time
+*
+* @author (c) Zbynek Winkler 2004 <zw at robotika cz>
+* 
+* $Id$
+*/
+
+#include "util/unit-test.h"
+#include "time.h"
+using namespace num;
+
+AUTOTEST(test)
+{
+}
+
