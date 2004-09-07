28e97096a7378cae5a5e043573e0efbd8e569bcb
zwin <zwin@72742d07-e5df-0310-90a3-f1be50577a5f>
2004-09-07 23:12:13 +0000

    Compiles and runs one dummy test.

--- branches/ester-merge/ester/field.cpp
+++ branches/ester-merge/ester/field.cpp
@@ -1,33 +1,60 @@
 #include "field.h"
+#include "measures.h"
+#include "util/assert.h"
 
-	using namespace measures;
-	ASSERT( m_side == 0 || m_side == 1 );
-	if (in_flags & RAND_PALM) randPalm(0); else setPalm(2,2);
-	if (in_flags & RAND_BALL)	setBall(0); else setBall(2,6,11);
-	if (in_flags & ERROR)	setError();
+#include <algorithm>
 
-void EsterSimul::setBall(uint32_t in_seed) //{{{1
-{
-	using namespace measures;
-	m_ballSeed = in_seed;
-	RandGen rnd;
-	if (m_ballSeed == 0)
-		m_ballSeed = time(NULL);
-	rnd.setSeed(m_ballSeed);
-	const int placeCount = BALL_POSITIONS - 2; // without 1 preset and 1 palm
-	int numbers[BALL_POSITIONS];
+using namespace measures;
+using namespace num;
+using namespace std;
 
-	// Prepare cards with positions
-	for (int i=0; i < placeCount; i++)
+namespace {
+
+	const int BALL_POSITIONS = 15;
+	const int PALM_POSITIONS = 10;
+	class RandGen : public Rnd //{{{1
 	{
-		numbers[i] = i;
-	}
-	random_shuffle(numbers, numbers+placeCount, rnd);
-	sort(numbers, numbers+3); // Sort first three (normalize)
-	setBall(numbers[0], numbers[1], numbers[2]);
+		public:
+			int operator()(int in_max)
+			{
+				//return (int) ((uniformD()+1)/2 * in_max);
+				return rand() % in_max; 
+      }
+	};
+
+	struct Coords //{{{1
+	{
+		Coords() {}
+		Coords(int in_x, int in_y) : x(in_x), y(in_y) {}
+		int x, y;
+	};
+	//}}}
 }
 
-void EsterSimul::randPalm(uint32_t in_seed) //{{{1
+Field::Field(int in_flags) //{{{1
+{
+	if (in_flags & RAND_PALM) randPalm(0); else setPalm(2,2);
+	if (in_flags & RAND_BALL)	setBall(0); else setBall(2,6,11);
+	if (in_flags & VERBOSE) m_verbose = true;
+}
+
+void Field::setPalm(int in_crossingX, int in_crossingY) //{{{1
+{
+	ASSERT( in_crossingX >= 2 && in_crossingX <=6 );
+	ASSERT( in_crossingY >= 2 && in_crossingY <=5 );
+	ASSERT( !(in_crossingX == 2 && in_crossingY == 5) && !(in_crossingX ==6 && in_crossingY == 2) );
+	
+	// Normalize palm position
+	if (in_crossingX > 4 || (in_crossingX == 4 && in_crossingY > 3 ))
+	{
+		in_crossingX = 8 - in_crossingX;
+		in_crossingY = 7 - in_crossingY;
+	}
+	m_palm1 = Point(measures::TILE() * in_crossingX, measures::TILE() * in_crossingY );
+	m_palm2 = Point(measures::TILE() * (8 - in_crossingX), measures::TILE() * (7 - in_crossingY));
+}
+
+void Field::randPalm(uint32_t in_seed) //{{{1
 {
 	m_palmSeed = in_seed;
 	if (m_palmSeed == 0)
@@ -53,7 +80,27 @@ void EsterSimul::randPalm(uint32_t in_seed) //{{{1
 	setPalm(cards[0].x, cards[1].y);
 }
 
-void EsterSimul::setBall(int in_pos1, int in_pos2, int in_pos3) //{{{1
+void Field::setBall(uint32_t in_seed) //{{{1
+{
+	m_ballSeed = in_seed;
+	RandGen rnd;
+	if (m_ballSeed == 0)
+		m_ballSeed = time(NULL);
+	rnd.setSeed(m_ballSeed);
+	const int placeCount = BALL_POSITIONS - 2; // without 1 preset and 1 palm
+	int numbers[BALL_POSITIONS];
+
+	// Prepare cards with positions
+	for (int i=0; i < placeCount; i++)
+	{
+		numbers[i] = i;
+	}
+	random_shuffle(numbers, numbers+placeCount, rnd);
+	sort(numbers, numbers+3); // Sort first three (normalize)
+	setBall(numbers[0], numbers[1], numbers[2]);
+}
+
+void Field::setBall(int in_pos1, int in_pos2, int in_pos3) //{{{1
 {
 	using namespace measures;
 	Coords cards[BALL_POSITIONS];
@@ -68,7 +115,7 @@ void EsterSimul::setBall(int in_pos1, int in_pos2, int in_pos3) //{{{1
 		Coords position((i / 6) + 2, (i % 6) + 1);
 		if (position.x == 2 && position.y == 5)
 			continue; // skip fixed ball positions
-		if ((position.x*TILE()).eq(m_palmPosition1.x()) && (position.y*TILE()).eq(m_palmPosition1.y()))
+		if ((TILE()*position.x).eq(m_palm1.x()) && (TILE()*position.y).eq(m_palm1.y()))
 			continue; // skip position where are the palms
 		cards[curCard++] = position;
 	}
@@ -76,55 +123,24 @@ void EsterSimul::setBall(int in_pos1, int in_pos2, int in_pos3) //{{{1
 
 	m_balls.clear();
 
-	m_balls.push_back(Place(measures::TILE() * 2, measures::TILE() * 5 ));;
-	m_balls.push_back(Place(measures::TILE() * (8 - 2) , 
+	m_balls.push_back(Point(measures::TILE() * 2, measures::TILE() * 5 ));;
+	m_balls.push_back(Point(measures::TILE() * (8 - 2) , 
 													measures::TILE() * (7 - 5)));
 
-	m_balls.push_back(Place(measures::TILE() * cards[in_pos1].x, measures::TILE() * cards[in_pos1].y ));
-	m_balls.push_back(Place(measures::TILE() * (8 - cards[in_pos1].x) , 
+	m_balls.push_back(Point(measures::TILE() * cards[in_pos1].x, measures::TILE() * cards[in_pos1].y ));
+	m_balls.push_back(Point(measures::TILE() * (8 - cards[in_pos1].x) , 
 													measures::TILE() * (7-cards[in_pos1].y )));
 
-	m_balls.push_back(Place(measures::TILE() * cards[in_pos2].x, measures::TILE() * cards[in_pos2].y ));
-	m_balls.push_back(Place(measures::TILE() * (8 - cards[in_pos2].x) , 
+	m_balls.push_back(Point(measures::TILE() * cards[in_pos2].x, measures::TILE() * cards[in_pos2].y ));
+	m_balls.push_back(Point(measures::TILE() * (8 - cards[in_pos2].x) , 
 													measures::TILE() * (7-cards[in_pos2].y )));
 
-	m_balls.push_back(Place(measures::TILE() * cards[in_pos3].x, measures::TILE() * cards[in_pos3].y ));
-	m_balls.push_back(Place(measures::TILE() * (8 - cards[in_pos3].x) , 
+	m_balls.push_back(Point(measures::TILE() * cards[in_pos3].x, measures::TILE() * cards[in_pos3].y ));
+	m_balls.push_back(Point(measures::TILE() * (8 - cards[in_pos3].x) , 
 													measures::TILE() * (7-cards[in_pos3].y )));
 }
 
-void EsterSimul::setPalm(int in_crossingX, int in_crossingY) //{{{1
-{
-	ASSERT( in_crossingX >= 2 && in_crossingX <=6 );
-	ASSERT( in_crossingY >= 2 && in_crossingY <=5 );
-	ASSERT( !(in_crossingX == 2 && in_crossingY == 5) && !(in_crossingX ==6 && in_crossingY == 2) );
-	
-	// Normalize palm position
-	if (in_crossingX > 4 || (in_crossingX == 4 && in_crossingY > 3 ))
-	{
-		in_crossingX = 8 - in_crossingX;
-		in_crossingY = 7 - in_crossingY;
-	}
-	m_palmPosition1 = Place(measures::TILE() * in_crossingX, measures::TILE() * in_crossingY );
-	m_palmPosition2 = Place(measures::TILE() * (8 - in_crossingX) , 
-			                    measures::TILE() * (7 - in_crossingY));
-}
-
-void EsterSimul::printStat() //{{{1
-{
-#if 0
-	cout << "*** Final score was " << m_score << ", balls: on the field " << m_balls.size() << ", in robot " 
-		<< m_numBallsIn << endl;
-	cout << "*** Last hit scored at " << m_lastHitTime.ms() / 1000.0 << " s" << endl;
-	cout << "*** Min palm dist was " << m_minPalmDist.mm() << " mm at [" << m_minPalmPose.n() << 
-		"] at " << m_minPalmTime.ms()/1000.0 << " s" << endl;
-	if (m_checkEnemy)
-	  cout << "*** Min enemy dist was " << m_minEnemyDist.mm() << " mm at [" << m_minEnemyPose.n() << 
-		  "] at " << m_minEnemyTime.ms()/1000.0 << " s" << endl;
-#endif
-}
-
-void EsterSimul::printIntro() //{{{1
+void Field::printIntro() //{{{1
 {
 #if 0
 	cout << "*** Using palm seed: " << m_palmSeed << endl;
@@ -138,31 +154,44 @@ void EsterSimul::printIntro() //{{{1
 #endif
 }
 
-void EsterSimul::checkPalms() //{{{1
+void Field::printStat() //{{{1
 {
-	if (m_pose.m_pose.distanceTo(m_palmPosition1) < m_minPalmDist)
+#if 0
+	cout << "*** Final score was " << m_score << ", balls: on the field " << m_balls.size() << ", in robot " 
+		<< m_numBallsIn << endl;
+	cout << "*** Last hit scored at " << m_lastHitTime.ms() / 1000.0 << " s" << endl;
+	cout << "*** Min palm dist was " << m_minPalmDist.mm() << " mm at [" << m_minPalmPose.n() << 
+		"] at " << m_minPalmTime.ms()/1000.0 << " s" << endl;
+	if (m_checkEnemy)
+	  cout << "*** Min enemy dist was " << m_minEnemyDist.mm() << " mm at [" << m_minEnemyPose.n() << 
+		  "] at " << m_minEnemyTime.ms()/1000.0 << " s" << endl;
+#endif
+}
+
+void Field::checkPalms(const Point & in_p) //{{{1
+{
+	if (in_p.distanceTo(m_palm1) < m_minPalmDist)
 	{
-		m_minPalmDist = m_pose.m_pose.distanceTo(m_palmPosition1);
-		m_minPalmPose = m_pose.m_pose;
-		m_minPalmTime = m_timeChange.m_time;
+		m_minPalmDist = in_p.distanceTo(m_palm1);
+		m_minPalmPose = in_p;
+		//m_minPalmTime = m_timeChange.m_time;
 	}
-	if (m_pose.m_pose.distanceTo(m_palmPosition2) < m_minPalmDist)
+	if (in_p.distanceTo(m_palm2) < m_minPalmDist)
 	{
-		m_minPalmDist = m_pose.m_pose.distanceTo(m_palmPosition2);
-		m_minPalmPose = m_pose.m_pose;
-		m_minPalmTime = m_timeChange.m_time;
+		m_minPalmDist = in_p.distanceTo(m_palm2);
+		m_minPalmPose = in_p;
+		//m_minPalmTime = m_timeChange.m_time;
 	}
 }
 
-void EsterSimul::checkEnemy() //{{{1
+void Field::checkEnemy(const Point & in_p) //{{{1
 {
-  if (!m_checkEnemy)
-    return ;
-  if (m_pose.m_pose.distanceTo(m_enemyPosition) < m_minEnemyDist)
+  if (in_p.distanceTo(m_enemy) < m_minEnemyDist)
   {
-	  m_minEnemyDist = m_pose.m_pose.distanceTo(m_palmPosition1);
-	  m_minEnemyPose = m_pose.m_pose;
-	  m_minEnemyTime = m_timeChange.m_time;
+	  m_minEnemyDist = in_p.distanceTo(m_enemy);
+	  m_minEnemyPose = in_p;
+	  //m_minEnemyTime = m_timeChange.m_time;
   }
 }
+//}}}1
 
--- branches/ester-merge/ester/field.h
+++ branches/ester-merge/ester/field.h
@@ -2,32 +2,48 @@
 #define ESTER_FILED_H_INCLUDED 1
 
 #include "number/pose.h"
+#include "number/dist.h"
+#include "number/time.h"
+
 #include <list>
 
 class Field
 {
 	private:
-		num::Pose m_enemy;
+		num::Point m_enemy;
 		int m_score;
-		num::Point m_palms[2];
+		num::Point m_palm1;
+		num::Point m_palm2;
 		std::list<num::Point> m_balls;
 		
 		uint32_t m_ballSeed;
 		uint32_t m_palmSeed;
+		bool m_verbose;
 
-		num::Dist m_minPalmDist;
-		num::Pose m_minPalmPose;
-		num::Time m_minPalmTime;
-		num::Dist m_minEnemyDist;
-		num::Pose m_minEnemyPose;
-		num::Time m_minEnemyTime;
-		num::Time m_lastHitTime;
+		num::Dist  m_minPalmDist;
+		num::Point m_minPalmPose;
+		num::Time  m_minPalmTime;
+		num::Dist  m_minEnemyDist;
+		num::Point m_minEnemyPose;
+		num::Time  m_minEnemyTime;
+		num::Time  m_lastHitTime;
 	
 		enum { BALL_COUNT=2*4, RAND_PALM=1, RAND_BALL=2, ERROR=4, VERBOSE=8 };
 
+		void printIntro();
+		void printStat();
+		
 	public:
 		Field(int in_flags);
 		~Field();
+		void setPalm(int in_crossingX = 0, int in_crossingY = 0);
+		void randPalm(uint32_t in_seed = 0);
+		void setBall(uint32_t in_seed = 0);
+		void setBall(int in_pos1, int in_pos2, int in_pos3);
+		void setEnemy(const num::Point& m_place);
+
+		void checkPalms(const num::Point & in_p);
+		void checkEnemy(const num::Point & in_p);
 };
 
 #endif // ESTER_FILED_H_INCLUDED
