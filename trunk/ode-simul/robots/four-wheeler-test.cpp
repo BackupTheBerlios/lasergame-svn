84a14a667556f780fc48aef8fd91ec37cf4fdf41
jup <jup@72742d07-e5df-0310-90a3-f1be50577a5f>
2004-11-23 23:51:18 +0000

    Simul test splitted to simul-test (just tests main loop) and separated test for four-wheeler robot. Also, test four four-wheeler robot now checks current-speed and actually succeeds.

---
+++ trunk/ode-simul/robots/four-wheeler-test.cpp
@@ -0,0 +1,96 @@
+// $Id$
+// Copyright (C) 2004, Zbynek Winkler
+
+#include <iostream>
+using namespace std;
+
+#include "../simul.h"
+#include "four-wheeler.h"
+#include "util/assert.h"
+#include "util/unit-test.h"
+using namespace num;
+using namespace msg;
+
+namespace {
+
+AUTOTEST(testLinearSpeed) //{{{1
+{
+	Subs<int> dir(0);
+	Subs<Time> dt(dir, "time-change");
+	Subs<int> watchdog(dir, "watchdog");
+	Subs<Speed> cur(dir, "speed-current");
+	Subs<Speed> req(dir, "speed-requested");
+
+	OdeSimul* simul = new OdeSimul(dir);
+	OdeRobot* bot = new FourWheeler(dir);
+	simul->addRobot(bot);
+	Task e(simul);
+	waitFor(dt);
+	int i = 0;
+
+	// accelerate
+	Time t = MSec(500);
+	req.value.m_forward = Milim(300);
+	//cout << req.value.m_forward.mm() << endl; 
+	req.publish();
+	while( t.gt() && cur.value.m_forward.lt(LinearSpeed(Milim(250))) )
+	{
+		t -= dt.value;
+		i++;
+		//cout << t.ms() << ": " << cur.value.m_forward.mm() << endl;
+		watchdog.publish();
+		waitFor(dt);
+	}
+	//cout << endl;
+	REQUIRE( !cur.value.m_forward.lt(LinearSpeed(Milim(250))) );
+
+	// decelerate
+	t = MSec(600);
+	req.value.m_forward = Milim(0);
+	req.publish();
+	while( t.gt() && cur.value.m_forward.gt() )
+	{
+		t -= dt.value;
+		i++;
+		//cout << t.ms() << ": " << currentSpeed.m_v.mm() << endl;
+		watchdog.publish();
+		waitFor(dt);
+	}
+	REQUIRE( !cur.value.m_forward.gt() );
+	//REQUIRE( i == 132); // 136
+}
+
+#if 0
+
+AUTOTEST(testMove) //{{{1
+{
+	Subs<int> dir(0);
+	Subs<Time> dt(dir, "time-change");
+	Subs<int> watchdog(dir, "watchdog");
+	Subs<Speed> req(dir, "speed-requested");
+	
+	OdeSimul* simul = new OdeSimul(dir);
+	OdeRobot* bot = new FourWheeler(dir);
+	simul->addRobot(bot);
+	Task e(simul);
+	waitFor(dt);
+	int i = 0;
+	
+	Time t = Sec(2);
+	Meter d(1);
+	req.value.m_forward = Milim(1070);
+	req.publish();
+	while( t.gt() && d.gt() )
+	{
+		t -= dt.value;
+		i++;
+		watchdog.publish();
+		waitFor(dt);
+		//cout << t.ms() << ": " << d.mm() << endl;
+	}
+	REQUIRE( i == 241 );
+}
+
+#endif
+
+}
--- trunk/ode-simul/simul-test.cpp
+++ trunk/ode-simul/simul-test.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 #include "simul.h"
-#include "robots/four-wheeler.h"
 #include "util/assert.h"
 #include "util/unit-test.h"
 using namespace num;
@@ -21,8 +20,6 @@ AUTOTEST(testTime) //{{{1
 	Subs<Time> dt(dir, "time-change");
 	Subs<int> watchdog(dir, "watchdog");
 	OdeSimul* simul = new OdeSimul(dir);
-	OdeRobot* bot = new FourWheeler(dir);
-	simul->addRobot(bot);
 	Task e(simul);
 	int i = 0;
 	for ( ; i < 10; i++)
@@ -34,33 +31,5 @@ AUTOTEST(testTime) //{{{1
 	REQUIRE( i == 10 );
 }
 
-AUTOTEST(testMove) //{{{1
-{
-	Subs<int> dir(0);
-	Subs<Time> dt(dir, "time-change");
-	Subs<int> watchdog(dir, "watchdog");
-	Subs<Speed> req(dir, "speed-requested");
-	
-	OdeSimul* simul = new OdeSimul(dir);
-	OdeRobot* bot = new FourWheeler(dir);
-	simul->addRobot(bot);
-	Task e(simul);
-	waitFor(dt);
-	int i = 0;
-	
-	Time t = Sec(2);
-	Meter d(1);
-	req.value.m_forward = Milim(1070);
-	req.publish();
-	while( t.gt() && d.gt() )
-	{
-		t -= dt.value;
-		i++;
-		watchdog.publish();
-		waitFor(dt);
-		//cout << t.ms() << ": " << d.mm() << endl;
-	}
-	REQUIRE( i == 241 );
-}
 
 }
